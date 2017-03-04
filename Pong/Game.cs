703dd4aed98c7919a64838b81492614b6272d1ef
Ashley Davies-Lyons <ashley@daviesl.uk>
2017-03-04 12:06:14 +0000

    Added initial project files with basic window setup and natives for x64 Windows

---
+++ Pong/Game.cs
@@ -0,0 +1,20 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace Pong {
+    public class Game {
+        private SFML.Window.Window window;
+
+        public void Start() {
+            window = new RenderWindow(new VideoMode(1200, 800), "Pong");
+
+            while (true) {
+                
+            }
+        }
+
+        public static void Main(string[] args) {
+            new Game().Start();
+        }
+    }
+}
